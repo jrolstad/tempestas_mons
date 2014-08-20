fcf33b00b72ea4ba3b4bd311be7ac5de9ff54a6b
Josh Rolstad <jrolstad@gmail.com>
2014-08-20 14:17:25 -0700

    Adding model for road condition analysis

---
+++ tempestas_mons.web/Models/roadconditions/RoadConditionRangeAnalysisViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace tempestas_mons.web.Models.roadconditions
+{
+    public class RoadConditionRangeAnalysisViewModel
+    {
+        public string Month { get; set; }
+
+        public int Day { get; set; }
+
+        public int Range { get; set; }
+
+        public string Direction { get; set; }
+
+        public RoadConditionSummaryViewModel Summary { get; set; }
+    }
+}
