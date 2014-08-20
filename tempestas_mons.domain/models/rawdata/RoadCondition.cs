0d70712839781c213ad5035c764801e01a0cdf7c
Josh Rolstad <jrolstad@gmail.com>
2014-08-19 21:00:31 -0700

    Adding models to represent data in the file

---
+++ tempestas_mons.domain/models/rawdata/RoadCondition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tempestas_mons.domain.models.rawdata
+{
+    public class RoadCondition
+    {
+        public string MountainPassConditionArchiveID {get;set;}
+        public string DateUpdated {get;set;}
+        public string Temperature {get;set;}
+        public string Weather {get;set;}
+        public string RoadCondition {get;set;}
+        public string RestrictionText1 {get;set;}
+        public string RestrictionText2 { get; set; }
+    }
+}
---
+++ tempestas_mons.domain/models/rawdata/TravelTime.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tempestas_mons.domain.models.rawdata
+{
+    public class TravelTime
+    {
+        public string Date_Time {get;set;}
+        public string DirectionTravelTimeMinutes {get;set;}
+        public string Free_Flow { get; set; }
+    }
+}
